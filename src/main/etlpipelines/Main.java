67e553a13a2034752325e8fc022a2533d4e07598
trungvuive <trung150502@gmail.com>
2024-09-03 04:23:12 +0700

    add taps and more operation

--- src/main/etlpipelines/Main.java
+++ src/main/etlpipelines/Main.java
@@ -11,7 +11,11 @@ import cascading.tap.hadoop.Hfs;
 import cascading.scheme.hadoop.TextDelimited;
 // import cascading.scheme.Scheme;
 
+import cascading.pipe.Each;
 import cascading.pipe.Pipe;
+import cascading.pipe.CoGroup;
+import cascading.pipe.GroupBy;
+import cascading.pipe.HashJoin;
 
 import cascading.flow.FlowDef;
 
@@ -52,15 +56,36 @@ public class Main {
 	public static void main(String[] args) {
 		String[] argsChecked = NullChecker.check(args, 4);
 
-		String documentPath = args[0], countPath = args[1];
-		String stopWordPath = args[2], resultPath = args[3];
+		String documentPath = args[0], countPath = args[1],
+				stopWordPath = args[2], resultPath = args[3];
 
 		// var tsvData = 
 
-		Tap sourceTap = new Hfs(new TextDelimited(true, "\t" ), inFilePath);
-		Tap destTap = new Hfs(new TextDelimited(true, "\t" ), outFilePath);
+		Tap documentTap = new Hfs(new TextDelimited(true, "\t" ), documentPath);
+		Tap countTap = new Hfs(new TextDelimited(true, "\t" ), countPath);
+		Tap resultTap = new Hfs(new TextDelimited( true, "\t" ), resultPath);
+		Tap stopTap = new Hfs(new TextDelimited(new Fields( "stop" ), true, "\t"), stopPath);
 
-		Pipe copyPipe = new Pipe("copy");
+		// regex operation to split the "document" into a token stream
+		Fields token = new Fields("token"), text = new Fields("text");
+		RegexSplitGenerator splitter = new RegexSplitGenerator( token, "[ \\[\\]\\(\\),.]" );
+		Fields fieldSelector = new Fields("doc_id", "token");
+
+		Pipe docPipe = new Each("token", text, splitter, fieldSelector);
+
+		// operation to clean up the token stream
+		Fields scrubArguments = new Fields("doc_id", "token");
+		docPipe = new Each(docPipe, scrubArguments, new Scrubber( scrubArguments ), Fields.RESULTS);
+
+		// token counts for document frequency-DF
+		Pipe dfPipe = new Unique("DF", tokenPipe, Fields.ALL);
+		Fields df_count = new Fields( "df_count" );
+		dfPipe = new CountBy( dfPipe, token, df_count );
+
+		Fields df_token = new Fields( "df_token" );
+		Fields lhs_join = new Fields( "lhs_join" );
+		dfPipe = new Rename( dfPipe, token, df_token );
+		dfPipe = new Each( dfPipe, new Insert( lhs_join, 1 ), Fields.ALL );
 
 		FlowDef flowDefine = FlowDef.flowDef().setName("File Copier").addSource(copyPipe, sourceTap).addTailSink(copyPipe, destTap);
 
