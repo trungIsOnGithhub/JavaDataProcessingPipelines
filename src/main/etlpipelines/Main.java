63675742863132058d362d2c93d42ae2cca0735f
trungvuive <trung150502@gmail.com>
2024-09-03 04:36:11 +0700

    add more pipe and operation

--- src/main/etlpipelines/Main.java
+++ src/main/etlpipelines/Main.java
@@ -73,21 +73,50 @@ public class Main {
 
 		Pipe docPipe = new Each("token", text, splitter, fieldSelector);
 
+		// perform a left join to remove stop words
+		Pipe stopPipe = new Pipe( "stop" );
+		Pipe tokenPipe = new HashJoin( docPipe, token, stopPipe, stop, new LeftJoin() );
+		tokenPipe = new Each( tokenPipe, stop, new RegexFilter( "^$" ) );
+		tokenPipe = new Retain( tokenPipe, fieldSelector );
+
 		// operation to clean up the token stream
 		Fields scrubArguments = new Fields("doc_id", "token");
 		docPipe = new Each(docPipe, scrubArguments, new Scrubber( scrubArguments ), Fields.RESULTS);
 
 		// token counts for document frequency-DF
 		Pipe dfPipe = new Unique("DF", tokenPipe, Fields.ALL);
-		Fields df_count = new Fields( "df_count" );
-		dfPipe = new CountBy( dfPipe, token, df_count );
+		Fields df_count = new Fields("df_count");
+		dfPipe = new CountBy(dfPipe, token, df_count);
 
-		Fields df_token = new Fields( "df_token" );
-		Fields lhs_join = new Fields( "lhs_join" );
+		Fields df_token = new Fields( "df_token" ),
+				lhs_join = new Fields( "lhs_join" );
 		dfPipe = new Rename( dfPipe, token, df_token );
 		dfPipe = new Each( dfPipe, new Insert( lhs_join, 1 ), Fields.ALL );
 
-		FlowDef flowDefine = FlowDef.flowDef().setName("File Copier").addSource(copyPipe, sourceTap).addTailSink(copyPipe, destTap);
+		// calculate number of documents
+		Fields doc_id = new Fields( "doc_id" );
+		Fields tally = new Fields( "tally" );
+		Fields rhs_join = new Fields( "rhs_join" );
+		Fields n_docs = new Fields( "n_docs" );
+		Pipe dPipe = new Unique( "D", tokenPipe, doc_id );
+		dPipe = new Each( dPipe, new Insert( tally, 1 ), Fields.ALL );
+		dPipe = new Each( dPipe, new Insert( rhs_join, 1 ), Fields.ALL );
+		dPipe = new SumBy( dPipe, rhs_join, tally, n_docs, long.class );
+
+		// calculate the TF-IDF weights, per token, per document
+		Fields tfidf = new Fields( "tfidf" );
+		String expression = "(double)tf_count * Math.log( (double) n_docs / ( 1.0 + df_count ) )";
+		ExpressionFunction tfidfExpression = new ExpressionFunction( tfidf, expression, Double.class );
+		Fields tfidfArguments = new Fields( "tf_count", "df_count", "n_docs" );
+		tfidfPipe = new Each( tfidfPipe, tfidfArguments, tfidfExpression, Fields.ALL );
+
+		fieldSelector = new Fields( "tf_token", "doc_id", "tfidf" );
+		tfidfPipe = new Retain( tfidfPipe, fieldSelector );
+		tfidfPipe = new Rename( tfidfPipe, tf_token, token );
+
+		FlowDef flowDefine = FlowDef.flowDef().setName("Data Pipelines Sample")
+											.addSource(docPipe, docTap).addSource(stopPipe, stopTap)
+											.addTailSink( tfidfPipe, resultTap ).addTailSink( wcPipe, countTap );
 
 		Properties properties = PropertiesGenerator.getApplicationConfigured();
 		FlowConnector flowConnector = new Hadoop2MR1FlowConnector(properties);
